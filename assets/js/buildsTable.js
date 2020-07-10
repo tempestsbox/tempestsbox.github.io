46122288afd1e3b1f5e785efe908803b3cece815
Daniel Orr <orrdaniel73@gmail.com>
2020-07-10 13:23:02 +0100

    Error catching and hover description for /downloads

--- assets/js/buildsTable.js
+++ assets/js/buildsTable.js
@@ -2,74 +2,94 @@ async function outputData(repo_user, repo_id, type, max_to_display, minified, sh
     var repo = `${repo_user}/${repo_id}`;
 
     console.log(`Fetching API data for repo ${repo}`);
-    var response = await fetch(`https://api.github.com/repos/${repo}/${type}`);
-    var data = await response.json();
+    fetch(`https://api.github.com/repos/${repo}/${type}`)
+        .then(async (response) => {
+            var data = await response.json();
+            var output = '';
 
-    let output = '';
-    data.slice(0, max_to_display)
-        .map((item, index) => {
-            let name;
-            let date;
-            let extra_content;
+            data.slice(0, max_to_display)
+                .map((item, index) => {
+                    let name;
+                    let date;
+                    let extra_content;
 
-            let name_link;
-            let download_link;
+                    let name_link;
+                    let download_link;
 
-            switch (type) {
-                case 'releases':
-                    date = item.published_at;
-                    split_body = item.body.split('\n');
+                    switch (type) {
+                        case 'releases':
+                            date = item.published_at;
+                            split_body = item.body.split('\n');
 
-                    extra_content = `
+                            extra_content = `
                 <td>${split_body[split_body.length - 1]}</td>
                     `
 
-                    name = item.name;
-                    download_link = `https://github.com/${repo}/archive/${item.tag_name}.zip`;
-                    name_link = `${window.location.origin}/article/release-${item.tag_name}`;
+                            name = item.name;
+                            download_link = `https://github.com/${repo}/archive/${item.tag_name}.zip`;
+                            name_link = `${window.location.origin}/article/release-${item.tag_name}`;
 
-                    break;
-                case 'commits':
-                    date = item.commit.committer.date;
+                            break;
+                        case 'commits':
+                            date = item.commit.committer.date;
 
-                    extra_content = `<td>${item.commit.message.split('\n')[0]}</td>`
+                            extra_content = `
+                    <td>
+                        <div class='tooltip'>
+                            ${item.commit.message.split('\n')[0]}
+                            <span class='tooltip-text no-highlight-anti'>
+                                ${item.commit.message.split('\n').join('<br>').split(' ').join('&nbsp;')}
+                            </span>
+                        </div>
+                    </td>`
 
-                    name = `<code>[${item.sha.substring(0, 7)}]</code>`;
-                    download_link = `https://github.com/${repo}/archive/${item.sha}.zip`;
-                    name_link = `https://github.com/tempestsbox/ttb/commit/${item.sha}`;
+                            name = `<code>[${item.sha.substring(0, 7)}]</code>`;
+                            download_link = `https://github.com/${repo}/archive/${item.sha}.zip`;
+                            name_link = `https://github.com/tempestsbox/ttb/commit/${item.sha}`;
 
-                    break;
-            }
+                            break;
+                    }
 
-            let content_date = '';
-            if (show_date != false) {
-                content_date = new Date(date).toISOString().split('T')[0];
-            } else {
-                name_link = download_link;
-            }
+                    let content_date = '';
+                    if (show_date != false) {
+                        content_date = new Date(date).toISOString().split('T')[0];
+                    } else {
+                        name_link = download_link;
+                    }
 
-            let tr = 'tr';
-            if (minified) tr += " class='minified'";
+                    let tr = 'tr';
+                    if (minified) tr += " class='minified'";
 
-            let content = `
-            <${tr}>
-                <td>
-                    <a href='${download_link}' style='border-bottom: none;'>
-                        <i class='fas fa-download'></i>
-                    </a>
-                    &nbsp;
-                    <a href='${name_link}' target='_blank' style='border-bottom: none;'>
-                        ${name}
-                    </a>
-                </td>
-                ${extra_content}
-                <td style='float: right;'>&nbsp;${content_date}</td>
-            </tr>
-            `
+                    let content = `
+                    <${tr}>
+                        <td>
+                            <a href='${download_link}' style='border-bottom: none;'>
+                                <i class='fas fa-download'></i>
+                            </a>
+                            &nbsp;
+                            <a href='${name_link}' target='_blank' style='border-bottom: none;'>
+                                ${name}
+                            </a>
+                        </td>
+                        ${extra_content}
+                        <td style='float: right;'>&nbsp;${content_date}</td>
+                    </tr>
+                    `
 
-            if (index != data.length - 1) content += '<br>';
-            output += (content);
-    });
+                    if (index != data.length - 1) content += '<br>';
+                    output += (content);
+                });
 
-    $(`.output.${repo_id}.${type}`).html(output);
+            $(`.output.${repo_id}.${type}`).html(output);
+        })
+        .catch(error => {
+            var output = `
+                <tr class='fetch-error'>
+                    <td>Error: ${error.message}</td>
+                </tr>
+            `;
+            console.log(error);
+    
+            $(`.output.${repo_id}.${type}`).html(output);
+        });
 }
